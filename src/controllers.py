a7c32752a63a7898e7cd306e48028ed0bf99737f
Go nakagawa <nakagawago0614@gmail.com>
2020-05-20 17:38:36 +0900

    modify admin func(Write session and throw template engien)

--- src/controllers.py
+++ src/controllers.py
@@ -2,6 +2,8 @@
 from fastapi import FastAPI
 from starlette.templating import Jinja2Templates
 from starlette.requests import Request
+import db
+from models import User, Task
 
 # FastAPI インスタンスの作成, レスポンス処理用
 app = FastAPI(
@@ -19,6 +21,13 @@ def index(request: Request):
                                       {'request': request})
 
 def admin(request: Request):
+    # ユーザとタスクを取得
+    # 現段階では、adminユーザのみを取得
+    user = db.session.query(User).filter(User.username == 'admin').first()
+    task = db.session.query(Task).filter(Task.user_id == user.id).all()
+    db.session.close()
+
     return templates.TemplateResponse('admin.html',
                                       {'request': request,
-                                       'username': 'admin'})
+                                       'user': user,
+                                       'task': task})
